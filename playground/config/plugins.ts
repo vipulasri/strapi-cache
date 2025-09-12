59679ae017ff2ff6ea5ae6564e5b8fb013917297
vipulasri <vipulasri.2007@gmail.com>
2025-09-12 13:01:43 -0400

    update: playground's config with excludeRoutes field

--- playground/config/plugins.ts
+++ playground/config/plugins.ts
@@ -9,6 +9,7 @@ export default ({ env }) => ({
       size: 1024 * 1024 * 1024, // Maximum size of the cache (1 GB) (only for memory cache)
       allowStale: false, // Allow stale cache items (only for memory cache)
       cacheableRoutes: [], // Caches routes which start with these paths (if empty array, all '/api' routes are cached)
+      excludeRoutes: [], // Do not cache routes which start with these paths (if empty array, no routes are excluded)
       provider: 'memory', // Cache provider ('memory' or 'redis')
       redisConfig: env('REDIS_URL', 'redis://localhost:6379'), // Redis config takes either a string or an object see https://github.com/redis/ioredis for references to what object is available, the object or string is passed directly to ioredis client (if using Redis)
       redisClusterNodes: [], // If provided any cluster node (this list is not empty), initialize ioredis redis cluster client. Each object must have keys 'host' and 'port'. See https://github.com/redis/ioredis for references
